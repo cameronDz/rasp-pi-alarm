cd2760452ad3ca1157a40f25b047bd4143dcb7a8
cameronDz <cameron.dziurgot@my.ccsu.edu>
2016-11-02 21:44:06 +0100

    Log class no longer abstract.

--- finalproject/src/edu/ccsu/cs417/finalproject/logger/Log.java
+++ finalproject/src/edu/ccsu/cs417/finalproject/logger/Log.java
@@ -1,19 +1,50 @@
 package edu.ccsu.cs417.finalproject.logger;
 
 /**
- * Class used as the bases for all widget logs
+ * Class used as the bases for all widget logs. Can also be a stand alone log.
+ * Currently no setters/getters since there does not seem to be any application
+ * for them.
  * @author Cameron
  */
-public abstract class Log {
+public class Log {
     protected int time;
     protected int date;
-    protected String widget;
+    protected String message;
+    
+    /**
+     * Constructor with no message; message variable set to "No Message."
+     * @param time Time the log occurred
+     * @param date Date the log occurred
+     */
+    public Log(int date, int time) {
+        this.date = date;
+        this.time = time;
+        message = "No message.";
+    }
+    
+    /**
+     * Constructor with a message for the Log.
+     * @param date Date the log occurred
+     * @param time Time the log occurred
+     * @param message message for informing the log
+     */
+    public Log(int date, int time, String message) {
+        this.date = date;
+        this.time = time;
+        this.message = message;
+    }
     
     /**
      * Print out that will be used to store log.
-     * @return Since widgets have unique data, String will be unique for each widget
+     * @return String representing Date, Time, and message of the Log.
      */
-    @Override
-    public abstract String toString(); 
+    @Override 
+    public String toString() {
+        String s = "";        
+        s += date + " ";
+        s += time + " ";
+        s += message;        
+        return s;
+    } 
     
 }
