a1ebcff8a69faad8e40644a0ca825a137531e62f
Chad <chadgit13@gmail.com>
2016-11-13 00:22:36 -0500

    Restore pull request files to master

---
+++ finalproject/led_off.py
@@ -0,0 +1,17 @@
+# Date: 11/6/16
+# Program to turn off the LED at port D4
+
+import sys
+sys.path.insert(0, 'GrovePi/Software/Python')
+
+import time
+from grovepi import *
+
+# Connect the Grove LED to digital port D4
+led = 4
+
+pinMode(led,"OUTPUT")
+time.sleep(1)
+
+#Turn off the LED
+digitalWrite(led,0)		# Send HIGH to switch off LED
--- finalproject/src/finalproject/Finalproject.java
+++ finalproject/src/finalproject/Finalproject.java
@@ -1,21 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package finalproject;
 
-/**
- *
- * @author Cameron
- */
+import edu.ccsu.cs417.finalproject.logger.Arm;
+import edu.ccsu.cs417.finalproject.logger.ArmAdapter;
+import edu.ccsu.cs417.finalproject.logger.Log;
+import edu.ccsu.cs417.finalproject.logger.LoggingService;
+import java.io.IOException;
+
 public class Finalproject {
 
     /**
      * @param args the command line arguments
+     * @throws InterruptedException
+     * @throws IOException
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws InterruptedException, IOException {
+        
+        // testing Log, LoggingService, LogCollection, and LogIterator
+        Log a = new Log(2112016,164600);
+        Log b = new Log(2112016,164700,"Has a message.");
+        Log c = new Log(2112016,164902);
+        
+        LoggingService.getInstance().addLog(a);
+        LoggingService.getInstance().addLog(b);
+        LoggingService.getInstance().addLog(c);
+        
+        String s = LoggingService.getInstance().getLogs();
+        System.out.println(s);
+        
+        Arm arm = new Arm();
+        ArmAdapter adapter = new ArmAdapter();
+        
+        // Activates LED by calling Python file
+        arm.activate();
+        System.out.println("Activated LED");
+        
+        // Pause 10 seconds before deactivating
+        Thread.sleep(10000);
+        
+        // Deactivates LED with adapter class which calls Python file
+        adapter.activate();
+        System.out.println("Deactivated LED");
     }
     
 }
