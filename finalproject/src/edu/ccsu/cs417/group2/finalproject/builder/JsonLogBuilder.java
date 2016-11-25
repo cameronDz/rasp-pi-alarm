be996a34b0b92e3277ed66bad2cfcce91b617437
cameronDz <cameron.dziurgot@my.ccsu.edu>
2016-11-25 09:55:56 +0000

    Implemented all classes to create the builder pattern.

--- finalproject/src/edu/ccsu/cs417/group2/finalproject/builder/JsonLogBuilder.java
+++ finalproject/src/edu/ccsu/cs417/group2/finalproject/builder/JsonLogBuilder.java
@@ -1,14 +1,62 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.ccsu.cs417.group2.finalproject.builder;
 
 /**
- *
+ * Converts a Log to String representation of JSON object
  * @author Cameron
  */
-public class JsonLogBuilder {
+public class JsonLogBuilder implements LogBuilder {
     
+    private String json;
+
+    /**
+     * Converts integer variable to JSON representation String
+     * @param time integer to be converted to a String
+     */
+    @Override
+    public void buildTime(int time) {
+        String s = "\"time\":\"" + time + "\"";
+        nullTest(s);
+    }
+
+    /**
+     * Converts integer variable to JSON representation String
+     * @param date integer to be converted to a String
+     */
+    @Override
+    public void buildDate(int date) {
+        String s = "\"date\":\"" + date + "\"";
+        nullTest(s);
+    }
+    
+    /**
+     * Converts String variable to JSON representation String
+     * @param message String to be converted to a String
+     */
+    @Override
+    public void buildMessage(String message) {
+        String s = "\"messsage\":\"" + message + "\"";
+        nullTest(s);
+    }
+    
+    /**
+     * Gets the current JSON string, closes it, and returns it
+     * @return JSON representation of the Log that was built
+     */
+    public String getJsonLog() {
+        String j = this.json;
+        j += "}";
+        return j;
+    }
+    
+    /**
+     * Test for start of JSON String, creates brackets if new String
+     * @param s String to be added to the JSON String
+     */
+    private void nullTest(String s) {
+        if(json == null) {
+            json = "{" + s;
+        } else {
+            json += s;
+        }
+    }
 }
--- finalproject/src/edu/ccsu/cs417/group2/finalproject/builder/LogReader.java
+++ finalproject/src/edu/ccsu/cs417/group2/finalproject/builder/LogReader.java
@@ -1,14 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.ccsu.cs417.group2.finalproject.builder;
 
+import edu.ccsu.cs417.group2.finalproject.logger.LogInterface;
+
 /**
- *
+ * Used to read logs and create objects using LogBuilder interface
  * @author Cameron
  */
 public class LogReader {
+    protected LogBuilder builder;
     
+    /**
+     * Contracted the LogReader to create a LogBuilder
+     * @param obj Object that will build a Log representation
+     */
+    public LogReader(LogBuilder obj) {
+        this.builder = obj;
+    }
+    
+    /**
+     * Builds a LogInterface through the LogBuilderInterface
+     * @param log 
+     */
+    public void parseLog(LogInterface log) {
+        this.builder.buildDate(log.getDate());
+        this.builder.buildTime(log.getTime());
+        this.builder.buildMessage(log.getMessage());
+    }
 }
