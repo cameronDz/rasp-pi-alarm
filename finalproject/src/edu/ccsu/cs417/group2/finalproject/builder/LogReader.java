2fbff59da37e2e033c4b5d077680f4af587c278a
cameronDz <cameron.dziurgot@my.ccsu.edu>
2016-11-25 09:51:50 +0000

    Added builder pattern UML and skeleton classes.

---
+++ finalproject/src/edu/ccsu/cs417/group2/finalproject/builder/LogReader.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.ccsu.cs417.group2.finalproject.builder;
+
+/**
+ *
+ * @author Cameron
+ */
+public class LogReader {
+    
+}
