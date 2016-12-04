588033e38f318a887303366478cf9163f277667d
CWillbanks <curtiswillbanks@my.ccsu.edu>
2016-12-04 14:08:20 -0500

    added properties file framework

--- finalproject/src/finalproject/Finalproject.java
+++ finalproject/src/finalproject/Finalproject.java
@@ -23,7 +23,8 @@ import edu.ccsu.cs417.group2.finalproject.logger.LogDecoratorFactory;
 import edu.ccsu.cs417.group2.finalproject.logger.WidgetLogDecoratorFactory;
 
 import edu.ccsu.cs417.group2.finalproject.state.SecuritySystem;
-
+import java.io.FileInputStream;
+import java.util.Properties;
 import java.io.IOException;
 
 public class Finalproject {
@@ -34,6 +35,38 @@ public class Finalproject {
      * @throws IOException
      */
     public static void main(String[] args) throws InterruptedException, IOException {
+        
+        FileInputStream environmentProperties;
+        
+        try {
+            environmentProperties = new FileInputStream("config/environment.properties");
+            Properties props = new Properties();
+            props.load(environmentProperties);
+            
+            if(props.containsKey(GREEN_LED_PORT)) {
+                // Component greenLed = new Led(props.getProperty(GREEN_LED_PORT));
+                System.out.println("Set green led");
+            }
+            if(props.containsKey(RED_LED_PORT)) {
+                // Component redLed = new Led(props.getProperty(RED_LED_PORT));
+                System.out.println("Set red led");
+            }
+            if(props.containsKey(BUZZER_PORT)) {
+                // Component buzzer = new Buzzer(props.getProperty(BUZZER_PORT));
+                System.out.println("Set buzzer");
+            }
+            if(props.containsKey(RANGER_PORT)) {
+                // Component ranger = new Ranger(props.getProperty(RANGER_PORT));
+                System.out.println("Set ranger");
+            }
+            if(props.containsKey(BUTTON_PORT)) {
+                // Component button = new Button(props.getProperty(BUTTON_PORT));
+                System.out.println("Set button");
+            }
+            
+        } catch (Exception ex) {
+          ex.printStackTrace();
+        }
         
         // testing Log, LoggingService, LogCollection, and LogIterator
         BasicLog a = new BasicLog(2112016,164600);
@@ -90,22 +123,24 @@ public class Finalproject {
         
         
         // Test activation of LED through adapter pattern
-        /*
         ArmLED armLED = new ArmLED();
         ArmAdapterLED adapterLED = new ArmAdapterLED();
         armLED.activate();
              // Pause 10 seconds before deactivating
         Thread.sleep(10000);
         adapterLED.activate();
-        */
 
         // Test activation of system through adapter pattern
-        /*
         Arm arm = new Arm();
         ArmAdapter adapter = new ArmAdapter();
         arm.activate();
         adapter.activate();
-        */
         
     }
+    
+    private final static String GREEN_LED_PORT = "GREEN_LED_PORT";
+    private final static String RED_LED_PORT   = "RED_LED_PORT";
+    private final static String BUZZER_PORT    = "BUZZER_PORT";
+    private final static String RANGER_PORT    = "RANGER_PORT";
+    private final static String BUTTON_PORT    = "BUTTON_PORT";
 }
