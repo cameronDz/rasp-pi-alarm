c3f6af2d18a4187920aa49fe0f83361ed90de48b
cameronDz <camerondziurgot@gmail.com>
2018-12-01 02:01:01 -0500

    Removed comments and some naming convention.

--- src/test/java/edu/ccsu/cs417/dgt/user/UserAdminDecoratorTest.java
+++ src/test/java/edu/ccsu/cs417/dgt/user/UserAdminDecoratorTest.java
@@ -11,24 +11,16 @@ import static org.junit.Assert.*;
  */
 public class UserAdminDecoratorTest {
 
-	/**
-	 * Test of getName method, of class UserAdminDecorator.
-	 */
 	@Test
 	public void testGetName() {
-		System.out.println("getName");
 		UserAdminDecorator instance = new UserAdminDecorator(new BasicUser("Tom"));
 		String expResult = "Tom";
 		String result = instance.getName();
 		assertEquals(expResult, result);
 	}
 
-	/**
-	 * Test of setName method, of class UserAdminDecorator.
-	 */
 	@Test
 	public void testSetName() {
-		System.out.println("setName");
 		String name = "Sarah";
 		UserAdminDecorator instance = new UserAdminDecorator(new BasicUser("Martha"));
 		instance.setName(name);
@@ -36,24 +28,16 @@ public class UserAdminDecoratorTest {
 		assertEquals(name, result);
 	}
 
-	/**
-	 * Test of getStatus method, of class UserAdminDecorator.
-	 */
 	@Test
 	public void testGetStatus() {
-		System.out.println("getStatus");
 		UserAdminDecorator instance = new UserAdminDecorator(new BasicUser("Karen"));
 		String expResult = "Administrator";
 		String result = instance.getStatus();
 		assertEquals(expResult, result);
 	}
 
-	/**
-	 * Test of setStatus method, of class UserAdminDecorator.
-	 */
 	@Test
 	public void testSetStatus() {
-		System.out.println("setStatus");
 		UserAdminDecorator instance = new UserAdminDecorator(new BasicUser("Jason"));
 		String expResult = "Moderator";
 		instance.setStatus("Moderator");
@@ -61,12 +45,8 @@ public class UserAdminDecoratorTest {
 		assertEquals(expResult, result);
 	}
 
-	/**
-	 * Test of getDecoratedUser method, of class UserAdminDecorator.
-	 */
 	@Test
 	public void testGetDecoratedUser() {
-		System.out.println("getDecoratedUser");
 		BasicUser becky = new BasicUser("Becky");
 		UserAdminDecorator instance = new UserAdminDecorator(becky);
 		UserInterface expResult = becky;
@@ -74,47 +54,31 @@ public class UserAdminDecoratorTest {
 		assertEquals(expResult, result);
 	}
 
-	/**
-	 * Test of toString method, of class UserAdminDecorator.
-	 */
 	@Test
 	public void testToString() {
-		System.out.println("toString");
 		UserAdminDecorator instance = new UserAdminDecorator(new BasicUser("Jason"));
 		String expResult = "UserAdminDecorator [BasicUser [name=Jason, parent=null] Administrator]";
 		String result = instance.toString();
 		assertEquals(expResult, result);
 	}
 
-	/**
-	 * Test of deleteLog method, of class UserAdminDecorator.
-	 */
 	@Test
 	public void testDeleteLog() {
-		System.out.println("deleteLog");
 		int i = 1;
 		UserAdminDecorator instance = new UserAdminDecorator(new BasicUser("Tom"));
 		instance.deleteLog(i);
 		assertTrue(LoggingService.getInstance().getLogs().equals(new LogCollection()));
 	}
 
-	/**
-	 * Test of hashCode method, of class UserAdminDecorator.
-	 */
 	@Test
 	public void testHashCode() {
-		System.out.println("hashCode");
 		UserAdminDecorator instance1 = new UserAdminDecorator(new BasicUser("Kelly"));
 		UserAdminDecorator instance2 = new UserAdminDecorator(new BasicUser("Kelly"));
 		assertTrue(instance1.hashCode() == instance2.hashCode());
 	}
 
-	/**
-	 * Testing Equals method with null object parameter
-	 */
 	@Test
 	public void testEqualsNull() {
-		System.out.println("equals");
 		UserAdminDecorator user = null;
 		UserAdminDecorator instance = new UserAdminDecorator(new BasicUser("Angela"));
 		boolean expResult = false;
@@ -122,24 +86,16 @@ public class UserAdminDecoratorTest {
 		assertEquals(expResult, result);
 	}
 
-	/**
-	 * Testing Equals method with same object as parameter
-	 */
 	@Test
 	public void testEqualsThis() {
-		System.out.println("equals");
 		UserAdminDecorator instance = new UserAdminDecorator(new BasicUser("Angela"));
 		boolean expResult = true;
 		boolean result = instance.equals(instance);
 		assertEquals(expResult, result);
 	}
 
-	/**
-	 * Testing Equals method with two objects with same name
-	 */
 	@Test
 	public void testEqualsPass() {
-		System.out.println("equals");
 		UserAdminDecorator user = new UserAdminDecorator(new BasicUser("Angela"));
 		UserAdminDecorator instance = new UserAdminDecorator(new BasicUser("Angela"));
 		boolean expResult = true;
@@ -147,12 +103,8 @@ public class UserAdminDecoratorTest {
 		assertEquals(expResult, result);
 	}
 
-	/**
-	 * Testing Equals method with two objects with different names
-	 */
 	@Test
 	public void testEqualsFail() {
-		System.out.println("equals");
 		UserAdminDecorator user = new UserAdminDecorator(new BasicUser("Barney"));
 		UserAdminDecorator instance = new UserAdminDecorator(new BasicUser("Angela"));
 		boolean expResult = false;
@@ -160,12 +112,8 @@ public class UserAdminDecoratorTest {
 		assertEquals(expResult, result);
 	}
 
-	/**
-	 * Testing getComposite method with two objects with different names
-	 */
 	@Test
 	public void testGetSetComposite() {
-		System.out.println("getComposite_setComposite");
 		UserComposite c = new UserListComposite("c");
 		UserInterface u = new BasicUser("Barney");
 		UserAdminDecorator instance = new UserAdminDecorator(u);
